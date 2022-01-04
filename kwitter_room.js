cb9b5ba20193a9fcc4a4388268b91c1240394511
ilikeHippos17 <91993463+ilikeHippos17@users.noreply.github.com>
2022-01-04 16:04:29 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -44,3 +44,28 @@ function redirectToRoomName(name)
   localStorage.setItem("room_name", name);
   window.location = "kwitter_page.html";
 }
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html"
+}
+
+function logout() {
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location = "kwitter.html";
+}
+
+function send()
+{
+  msg = document.getElementById("msg").ariaValueMax;
+  firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+  });
+
+  document.getElementById("msg").value = "";
+}
